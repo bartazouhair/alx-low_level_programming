a41be36eb5bf42968845b7ee4e67dfd20c826f79
bartazouhair <bartazouhair@gmail.com>
2023-03-16 13:26:32 -0700

    Task 8, UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line with puts function
+ * Description: Prints out a string to stdout.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
