c7a8c4b4d4acd9da6f25441d9a0c3377e39ce264
bartazouhair <bartazouhair@gmail.com>
2023-03-16 12:12:33 -0700

    Task 4, 4-Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* description:  main - A program that prints a line with puts function
+* Return : 0 (Succes)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
