92b60d512ecc7db46cb8cdcc7eeedc31ae242eb7
bartazouhair <bartazouhair@gmail.com>
2023-06-15 07:35:20 -0700

    Update Task 11

--- 0x17-doubly_linked_lists/100-result.py
+++ 0x17-doubly_linked_lists/100-result.py
@@ -4,7 +4,7 @@ def is_palindrome(n):
 largest_palindrome = 0
 
 for i in range(100, 1000):
-    for j in range(100, 1000):
+    for j in range(i, 1000):  # Éviter les duplications en utilisant range(i, 1000)
         product = i * j
         if is_palindrome(product) and product > largest_palindrome:
             largest_palindrome = product
